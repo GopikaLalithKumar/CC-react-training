856ef22e31e4cc1eaa097bfe77a10c229eba549f
Gopika R <gopika074@gmail.com>
2021-12-01 20:02:43 +0530

    push

---
+++ myfirstreact-app/session10a/src/app.tsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import List from "./components/List";
+import Search from "./components/Seacrh";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hacker Stories</h1>
+      <Search />
+      <List stories={list} />
+    </div>
+  );
+}
