9119c897d45e9ac711ce5c44384d7f4f871a0256
Gopika R <gopika074@gmail.com>
2021-12-01 17:44:51 +0530

    commit

---
+++ myapp/session10a/src/App.jsx
@@ -0,0 +1,31 @@
+import styles from "./App";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App(){
+  
+  return(
+
+<div >
+      <h1>welcome to </h1>
+    </div>
+  );
+}
+export default App;
